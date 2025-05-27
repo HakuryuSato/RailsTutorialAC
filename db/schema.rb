853056ebc48b71e9a27a043005d539fb3813a54d
Hakuryu Sato <hakuryu.sato@as-child.com>
2025-05-27 15:32:41 +0900

    micropostをmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_055704) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_063200) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
